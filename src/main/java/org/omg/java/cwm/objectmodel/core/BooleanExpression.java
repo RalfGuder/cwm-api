65388e75484243330998a96b4d722d06a2c6531c
RalfGuder <45797275+RalfGuder@users.noreply.github.com>
2024-01-16 08:13:14 +0100

    Update BooleanExpression.java

--- src/main/java/org/omg/java/cwm/objectmodel/core/BooleanExpression.java
+++ src/main/java/org/omg/java/cwm/objectmodel/core/BooleanExpression.java
@@ -1,11 +1,10 @@
-/*
- * CWM Version 1.0, Java(TM) Metadata Interface Version 1.0
- */
 package org.omg.java.cwm.objectmodel.core;
 
 
-
-public interface BooleanExpression
-extends org.omg.java.cwm.objectmodel.core.Expression {
+/**
+ * In the metamodel BooleanExpression defines a statement that will 
+ * evaluate to an instance of Boolean when it is evaluated.
+ */
+public interface BooleanExpression extends Expression {
 
 }
