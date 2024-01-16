a956115485057525330cfb34a88ae6842ca126f2
RalfGuder <45797275+RalfGuder@users.noreply.github.com>
2024-01-16 08:08:42 +0100

    Update Attribute.java

--- src/main/java/org/omg/java/cwm/objectmodel/core/Attribute.java
+++ src/main/java/org/omg/java/cwm/objectmodel/core/Attribute.java
@@ -1,35 +1,29 @@
-/*
- * CWM Version 1.0, Java(TM) Metadata Interface Version 1.0
- */
 package org.omg.java.cwm.objectmodel.core;
 
 
 /**
  * An Attribute describes a named slot within a Classifier that may hold a value.
  */
-public interface Attribute
-extends org.omg.java.cwm.objectmodel.core.StructuralFeature {
-
-	// ------------------------------------------------
-	// -----   Attribute-Generated                -----
-	// ------------------------------------------------
+public interface Attribute extends StructuralFeature {
 
 	/**
-	 * Read the <tt>initialValue</tt> property.
-	 * <p>
-	 * An Expression specifying the value of the attribute upon initialization. It is meant to be
+	 * Read the 'initialValue' property value.
+         *
+	 * <p>An Expression specifying the value of the attribute upon initialization. It is meant to be
 	 * evaluated at the time the object is initialized. (Note that an explicit constructor may supersede
 	 * an initial value.)
 	 */
-	public org.omg.java.cwm.objectmodel.core.Expression getInitialValue();
+	public Expression getInitialValue();
+	
 	/**
-	 * Write the <tt>initialValue</tt> property.
-	 * <p>
-	 * An Expression specifying the value of the attribute upon initialization. It is meant to be
+	 * Write the 'initialValue' property.
+         *
+	 * <p>An Expression specifying the value of the attribute upon initialization. It is meant to be
 	 * evaluated at the time the object is initialized. (Note that an explicit constructor may supersede
 	 * an initial value.)
+         *
 	 * @param value as Expression
 	 */
-	public void setInitialValue( org.omg.java.cwm.objectmodel.core.Expression value );
+	public void setInitialValue( Expression value );
 
 }
